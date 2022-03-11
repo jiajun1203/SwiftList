c2c91f3812046b7dd75066f3952f92a8a6c634d1
Mr.陈 <jiajun12_03@163.com>
2022-03-11 20:13:57 +0800

    Initial Commit

---
+++ OpenColseList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenColseList
+//
+//  Created by 陈征征 on 2022/3/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
